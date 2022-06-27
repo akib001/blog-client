c741cea78f3a5edcb45a8bc3d0debd7b498be7fe
Akib <akibahmed2011@gmail.com>
2022-06-28 00:57:25 +0600

    logo updated

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import './Logo.css';
 
-const logo = props => <h1 className="logo">Mini Blog</h1>;
+const logo = props => <h1 className="logo">Micro Blog</h1>;
 
 export default logo;
